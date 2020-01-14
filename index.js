85b310e1f4e3278d8c3824021200b797a17bf785
Afsin Ustundag <afsin_ustundag@yahoo.com>
2020-01-14 06:31:30 -0500

    create language model

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const Base = require('./lib/base');
 const Translatable = require('./lib/translatable');
 const LanguageDAO = require('./lib/language-dao');
+const Language = require('./lib/language-model');
 
 module.exports = {
     Base,
     Translatable,
     LanguageDAO,
+    Language,
 };
