b8fc45850f56a4946392fc31ef033447f5966979
Afsin Ustundag <afsin_ustundag@yahoo.com>
2020-01-13 23:51:05 -0500

    code cleaning

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const Base = require('./lib/base');
 const Translatable = require('./lib/translatable');
+const LanguageDAO = require('./lib/language-dao');
 
 module.exports = {
     Base,
     Translatable,
+    LanguageDAO,
 };
