db5878ae3e51d01e88d70e5cd4ca304245ce78ae
Afsin Ustundag <afsin_ustundag@yahoo.com>
2020-01-19 16:46:22 -0500

    move common methods

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const Base = require('./lib/base');
 const Translatable = require('./lib/translatable');
 const LanguageDAO = require('./lib/language-dao');
 const Language = require('./lib/language-model');
+const generator = require('./lib/generator');
 
 module.exports = {
     Base,
     Translatable,
     LanguageDAO,
     Language,
+    generateDb: generator.generateDb,
+    generateConfig: generator.generateConfig,
+    Sequelize: generator.Sequelize,
 };
